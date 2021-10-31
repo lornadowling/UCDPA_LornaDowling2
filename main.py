96211ec08f860dfac90bff2efaf3b24405475861
lornadowling <87702588+lornadowling@users.noreply.github.com>
2021-10-31 23:16:34 +0000

    print header data

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ import pandas as pd
 import numpy as np
 BTC_Sales_Data = pd.read_csv("BTC Sales data.csv")
 BTC_Sales_info = BTC_Sales_Data.info()
+BTC_Sales = BTC_Sales_Data.replace(0, np.NaN)
+print(BTC_Sales.head(10))
